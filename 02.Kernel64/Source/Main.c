c71962930f30549088f1aa53346c4bbfd52e19ea
KANG-build <yangsieun254@gmail.com>
2026-03-16 21:19:56 +0900

    update Main.c

--- 02.Kernel64/Source/Main.c
+++ 02.Kernel64/Source/Main.c
@@ -1,6 +1,7 @@
 #include "Types.h"
 #include "Keyboard.h"
 #include "Descriptor.h"
+#include "PIC.h"
 
 // 함수 선언
 void kPrintString( int iX, int iY, const char* pcString );
@@ -43,6 +44,14 @@ void Main( void )
         while( 1 ) ;
     }
 
+    kPrintString( 0, 16, "PIC Controller And Interrupt Initialize....[    ]" );
+    // PIC 컨트롤러 초기화 및 모든 인터럽트 활성화
+    kInitializePIC();
+    kMaskPICInterrupt( 0 );
+    kEnableInterrupt();
+
+    kPrintString( 45, 16, "Pass" );
+
     while( 1 )
     {
         // 출력 버퍼(포트 0x60)가 차 있으면 스캔 코드를 읽을 수 있음
@@ -58,7 +67,7 @@ void Main( void )
                 // 키가 눌렸으면 키의 ASCII 코드 값을 화면에 출력
                 if( bFlags & KEY_FLAGS_DOWN )
                 {
-                    kPrintString( i++, 16, vcTemp );
+                    kPrintString( i++, 17, vcTemp );
 
                     // '0'이 입력되면 Divide Error 예외(벡터 0번)를 발생시킴
                     if( vcTemp[ 0 ] == '0' )
