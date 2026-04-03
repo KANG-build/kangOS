55e1a16aac20a25144dd9c3ecd31819321132487
KANG-build <yangsieun254@gmail.com>
2026-04-03 16:07:36 +0900

    ch.14

--- 02.Kernel64/Source/InterruptHandler.c
+++ 02.Kernel64/Source/InterruptHandler.c
@@ -1,5 +1,6 @@
 #include "InterruptHandler.h"
 #include "PIC.h"
+#include "Keyboard.h"
 
 // 공통으로 사용하는 예외 핸들러
 void kCommonExceptionHandler( int iVectorNumber, QWORD qwErrorCode )
@@ -45,6 +46,7 @@ void kKeyboardHandler( int iVectorNumber )
 {
     char vcBuffer[] = "[INT:  , ]";
     static int g_iKeyboardInterruptCount = 0;
+    BYTE bTemp;
 
     // 인터럽트가 발생했음을 알리려고 메시지를 출력
     // 인터럽트 벡터를 화면 오른쪽 위에 2자리 정수로 출력
@@ -56,6 +58,13 @@ void kKeyboardHandler( int iVectorNumber )
     g_iKeyboardInterruptCount = ( g_iKeyboardInterruptCount + 1 ) % 10;
 
     kPrintString( 0, 0, vcBuffer );
+
+    // 키보드 컨트롤러에서 데이터를 읽어서 ASCII로 변환하여 큐에 삽입 
+    if( kIsOutputBufferFull() == TRUE )
+    {
+        bTemp = kGetKeyboardScanCode();
+        kConvertScanCodeAndPutQueue( bTemp );
+    }
 
     // EOI 전송
     kSendEOIToPIC( iVectorNumber - PIC_IRQSTARTVECTOR );
