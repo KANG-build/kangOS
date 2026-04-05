e47af5ba84ab5dfbccbfe5f54801a1fe429968e1
yangsieun <didtldms2525@naver.com>
2026-04-05 17:00:35 +0900

    ch.15

--- 02.Kernel64/Source/ConsoleShell.c
+++ 02.Kernel64/Source/ConsoleShell.c
@@ -92,7 +92,8 @@ void kExecuteCommand( const char* pcCommandBuffer )
 
     // 공백으로 구분된 커맨드를 추출 
     iCommandBufferLength = kStrLen( pcCommandBuffer );
-    for( iSpaceIndex = 0; iSpaceIndex < iCommandBufferLength ; iSpaceIndex++ ){
+    for( iSpaceIndex = 0; iSpaceIndex < iCommandBufferLength ; iSpaceIndex++ )
+    {
         if( pcCommandBuffer[ iSpaceIndex ] == ' ')
         {
             break;
@@ -111,151 +112,151 @@ void kExecuteCommand( const char* pcCommandBuffer )
             gs_vstCommandTable[i].pfFunction( pcCommandBuffer + iSpaceIndex + 1 );
             break;
         }
+    }
 
-        // 리스트에서 찾을 수 없다면 에러 출력 
-        if ( i >= iCount )
+    // 리스트에서 찾을 수 없다면 에러 출력 
+    if ( i >= iCount )
+    {
+        kPrintf( "'%s' is not found.\n", pcCommandBuffer );
+    }
+}
+
+// 파라미터 자료구조를 초기화 
+void kInitializeParameter( PARAMETERLIST* pstList, const char* pcParameter )
+{
+    pstList->pcBuffer = pcParameter;
+    pstList->iLength = kStrLen( pcParameter );
+    pstList->iCurrentPosition =0;
+}
+
+// 공백으로 구분된 파라미터의 내용과 길이를 반환 
+int kGetNextParameter( PARAMETERLIST* pstList, char* pcParameter )
+{
+    int i;
+    int iLength;
+
+    // 더 이상 파라미터가 없으면 나감 
+    if( pstList->iLength <= pstList->iCurrentPosition )
+    {
+        return 0;
+    }
+
+    // 버퍼의 길이만큼 이동하면서 공백을 검색 
+    for( i = pstList->iCurrentPosition ; i<pstList->iLength; i++ )
+    {
+        if( pstList->pcBuffer[i] == ' ')
         {
-            kPrintf( "'%s' is not found.\n", pcCommandBuffer );
+            break;
         }
     }
 
-    // 파라미터 자료구조를 초기화 
-    void kInitializeParameter( PARAMETERLIST* pstList, const char* pcParameter )
+    // 파라미터를 복사하고 길이를 반환 
+    kMemCpy( pcParameter, pstList->pcBuffer + pstList->iCurrentPosition, i );
+    iLength = i - pstList->iCurrentPosition; 
+    pcParameter[ iLength ] = '\0';
+
+    // 파라미터의 위치 업데이터 
+    pstList->iCurrentPosition += iLength + 1;
+    return iLength;
+}
+
+void kHelp( const char* pcCommandBuffer )
+{
+    int i;
+    int iCount; 
+    int iCursorX, iCursorY; 
+    int iLength, iMaxCommandLength = 0;
+
+    kPrintf( "=========================================================\n" );
+    kPrintf( "                     KANG64 Shell Help                   \n" );
+    kPrintf( "=========================================================\n" );
+
+    iCount = sizeof( gs_vstCommandTable ) / sizeof( SHELLCOMMANDENTRY );
+
+    // 가장 긴 커맨드의 길이를 계산
+    for( i = 0 ; i < iCount ; i++ )
     {
-        pstList->pcBuffer = pcParameter;
-        pstList->iLength = kStrLen( pcParameter );
-        pstList->iCurrentPosition =0;
-    }
-
-    // 공백으로 구분된 파라미터의 내용과 길이를 반환 
-    int kGetNextParameter( PARAMETERLIST* pstList, char* pcParameter )
-    {
-        int i;
-        int iLength;
-
-        // 더 이상 파라미터가 없으면 나감 
-        if( pstList->iLength <= pstList->iCurrentPosition )
+        iLength = kStrLen( gs_vstCommandTable[ i ].pcCommand );
+        if( iLength > iMaxCommandLength )
         {
-            return 0;
-        }
-
-        // 버퍼의 길이만큼 이동하면서 공백을 검색 
-        for( i = pstList->iCurrentPosition ; i<pstList->iLength; i++ )
-        {
-            if( pstList->pcBuffer[i] == ' ')
-            {
-                break;
-            }
-        }
-
-        // 파라미터를 복사하고 길이를 반환 
-        kMemCpy( pcParameter, pstList->pcBuffer + pstList->iCurrentPosition, i );
-        iLength = i - pstList->iCurrentPosition; 
-        pcParameter[ iLength ] = '\0';
-
-        // 파라미터의 위치 업데이터 
-        pstList->iCurrentPosition += iLength + 1;
-        return iLength;
-    }
-
-    void kHelp( const char* pcCommandBuffer )
-    {
-        int i;
-        int iCount; 
-        int iCursorX, iCursorY; 
-        int iLength, iMaxCommandLength = 0;
-
-        kPrintf( "=========================================================\n" );
-        kPrintf( "                     MINT64 Shell Help                   \n" );
-        kPrintf( "=========================================================\n" );
-
-        iCount = sizeof( gs_vstCommandTable ) / sizeof( SHELLCOMMANDENTRY );
-
-        // 가장 긴 커맨드의 길이를 계산
-        for( i = 0 ; i < iCount ; i++ )
-        {
-            iLength = kStrLen( gs_vstCommandTable[ i ].pcCommand );
-            if( iLength > iMaxCommandLength )
-            {
-                iMaxCommandLength = iLength;
-            }
-        }
-
-        // 도움말 출력
-        for( i = 0 ; i < iCount ; i++ )
-        {
-            kPrintf( "%s", gs_vstCommandTable[ i ].pcCommand );
-            kGetCursor( &iCursorX, &iCursorY );
-            kSetCursor( iMaxCommandLength, iCursorY );
-            kPrintf( " - %s\n", gs_vstCommandTable[ i ].pcHelp );
+            iMaxCommandLength = iLength;
         }
     }
 
-    // 화면을 지움 
-    void kCls( const char* pcParameterBuffer )
+    // 도움말 출력
+    for( i = 0 ; i < iCount ; i++ )
     {
-        // 맨 윗줄은 디버깅 용으로 사용하므로 화면을 지운 후 라인 1로 커서 이동 
-        kClearScreen();
-        kSetCursor( 0, 1 );
-
+        kPrintf( "%s", gs_vstCommandTable[ i ].pcCommand );
+        kGetCursor( &iCursorX, &iCursorY );
+        kSetCursor( iMaxCommandLength, iCursorY );
+        kPrintf( " - %s\n", gs_vstCommandTable[ i ].pcHelp );
     }
+}
 
-    // 총 메모리 크기를 출력 
-    void kShowTotalRAMSize( const char* pcParameterBuffer )
+// 화면을 지움 
+void kCls( const char* pcParameterBuffer )
+{
+    // 맨 윗줄은 디버깅 용으로 사용하므로 화면을 지운 후 라인 1로 커서 이동 
+    kClearScreen();
+    kSetCursor( 0, 1 );
+
+}
+
+// 총 메모리 크기를 출력 
+void kShowTotalRAMSize( const char* pcParameterBuffer )
+{
+    kPrintf( "Total RAM Size = %d MB\n", kGetTotalRAMSize());
+}
+
+// 문자열로 된 숫자를 숫자로 변환하여 화면에 출력 
+void kStringToDecimalHexTest( const char* pcParameterBuffer )
+{
+    char vcParameter[ 100 ];
+    int iLength;
+    PARAMETERLIST stList;
+    int iCount = 0;
+    long lValue;
+
+    // 파라미터 초기화 
+    kInitializeParameter( &stList, pcParameterBuffer );
+
+    while( 1 )
     {
-        kPrintf( "Total RAM Size = %d MB\n", kGetTotalRAMSize());
-    }
-
-    // 문자열로 된 숫자를 숫자로 변환하여 화면에 출력 
-    void kStringToDecimalHexTest( const char* pcParameterBuffer )
-    {
-        char vcParameter[ 100 ];
-        int iLength;
-        PARAMETERLIST stList;
-        int iCount = 0;
-        long lValue;
-
-        // 파라미터 초기화 
-        kInitializeParameter( &stList, pcParameterBuffer );
-
-        while( 1 )
+        // 다음 파라미터를 구함, 파라미터의 길이가 0이면 파라미터가 없는 것이므로 종료 
+        iLength = kGetNextParameter( &stList, vcParameter );
+        if( iLength == 0)
         {
-            // 다음 파라미터를 구함, 파라미터의 길이가 0이면 파라미터가 없는 것이므로 종료 
-            iLength = kGetNextParameter( &stList, vcParameter );
-            if( iLength == 0)
-            {
-                break;
-            }
-
-            // 파라미터에 대한 정보를 출력하고 16진수인지 10진수인지 판단하여 변환한 후 
-            // 결과를 printf로 출력 
-            kPrintf("Param %d = '%s', Length = %d, ", iCount + 1, 
-                    vcParameter, iLength );
-            
-            // 0x로 시작하면 16진수, 그외에는 10진수로 판단 
-            if( kMemCmp( vcParameter, "0x", 2) == 0)
-            {
-                lValue = kAToI( vcParameter + 2, 16);
-                kPrintf("HEX Value = %q\n", lValue);
-            }
-            else
-            {
-                lValue = kAToI( vcParameter, 10 );
-                kPrintf( "Decimal Value = %d\n", lValue );
-            }
-
-            iCount++;
+            break;
         }
-    }
 
-    // PC를 재시작
-    void kShutdown( const char* pcParamegerBuffer )
-    {
-        kPrintf( "System Shutdown Start...\n");
+        // 파라미터에 대한 정보를 출력하고 16진수인지 10진수인지 판단하여 변환한 후 
+        // 결과를 printf로 출력 
+        kPrintf("Param %d = '%s', Length = %d, ", iCount + 1, 
+                vcParameter, iLength );
+        
+        // 0x로 시작하면 16진수, 그외에는 10진수로 판단 
+        if( kMemCmp( vcParameter, "0x", 2) == 0)
+        {
+            lValue = kAToI( vcParameter + 2, 16);
+            kPrintf("HEX Value = %q\n", lValue);
+        }
+        else
+        {
+            lValue = kAToI( vcParameter, 10 );
+            kPrintf( "Decimal Value = %d\n", lValue );
+        }
 
-        // 키보드 컨트롤러를 통해 PC를 재시작
-        kPrintf( "Press Any Key To Reboot PC...");
-        kGetCh();
-        kReboot();
+        iCount++;
     }
+}
+
+// PC를 재시작
+void kShutdown( const char* pcParamegerBuffer )
+{
+    kPrintf( "System Shutdown Start...\n");
+
+    // 키보드 컨트롤러를 통해 PC를 재시작
+    kPrintf( "Press Any Key To Reboot PC...");
+    kGetCh();
+    kReboot();
 }
